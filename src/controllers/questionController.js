85a8b783ce2f55316aa4365390df181530f63cf3
jiya-22 <jiya220205@gmail.com>
2026-07-23 23:38:09 +0530

    Day 29: Added pagination to Question API and completed Question CRUD

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -30,15 +30,26 @@ const getQuestions = async (req, res) => {
                 $options: "i"
             };
         }
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const skip=(page-1)*limit;
+        const totalQuestions = await Question.countDocuments(filter);
+                const totalPages = Math.ceil(totalQuestions / limit);    
 
         const questions = await Question.find(filter)
-            .populate("companies", "companyName logo");
+            .populate("companies", "companyName logo")
+            .skip(skip)
+            .limit(limit);
 
         return res.status(200).json({
-            success: true,
-            count: questions.length,
-            data: questions
-        });
+    success: true,
+    page,
+    limit,
+    totalQuestions,
+    totalPages,
+    count: questions.length,
+    data: questions
+});
 
     } catch (error) {
 
